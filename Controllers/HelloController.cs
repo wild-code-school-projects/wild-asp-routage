8b27c425c657aaad83f46f98c25bdc6ef93c641d
SuperCoolNinja <sbpro570@gmail.com>
2024-05-21 15:12:37 +0200

    init

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routage.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/{controller}/Say")]
+        public IActionResult SayHello()
+        {
+            return Content("Say");
+        }
+
+        [Route("/{controller}/Yell")]
+        public IActionResult YellHello()
+        {
+            return Content("Yell");
+        }
+
+        [Route("Hello")]
+        public IActionResult Hello()
+        {
+            return Content("Say");
+        }
+
+        [Route("Yell")]
+        public IActionResult Yell()
+        {
+            return Content("Yell");
+        }
+    }
+}
